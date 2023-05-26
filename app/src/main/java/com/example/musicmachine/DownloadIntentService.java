6eea5720f941de9e92796e571adfa0ec7ee49437
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-05-26 23:55:53 +0530

    added downloadintent service'

--- app/src/main/java/com/example/musicmachine/DownloadIntentService.java
+++ app/src/main/java/com/example/musicmachine/DownloadIntentService.java
@@ -2,22 +2,35 @@ package com.example.musicmachine;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 public class DownloadIntentService extends IntentService {
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    public DownloadIntentService(String name) {
+    private static final String TAG = "DownloadIntentService";
+
+    public DownloadIntentService() {
         super("DownloadIntent Service");
         setIntentRedelivery(true);
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        assert intent != null;
+        String song = intent.getStringExtra(MainActivity.KEY_SONG);
+        try {
+            downLoadSong(song);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void downLoadSong(String songName) throws InterruptedException {
 
+        long endTime = System.currentTimeMillis() + 10 * 10000;
+        while(System.currentTimeMillis() <= endTime) {
+
+            Thread.sleep(1000);
+        }
+        Log.d(TAG, "downLoadSong: " +  songName);
     }
 }
--- app/src/main/java/com/example/musicmachine/MainActivity.java
+++ app/src/main/java/com/example/musicmachine/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             for (String song:
                  Playlist.songs) {
 
-                Intent intent = new Intent(MainActivity.this , DownloadService.class);
+                Intent intent = new Intent(MainActivity.this , DownloadIntentService.class);
                 intent.putExtra(KEY_SONG, song);
                 startService(intent);
 
